a0dca5a9f151a0d9711fb455d7617284c835dba6
thanhnx <thanhnx.tkh@gmail.com>
2024-09-09 08:42:20 +0700

    Initial commit

---
+++ Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int CategoryId { get; set; }
+
+    public Product()
+    {
+    }
+    public Product(int id, string name, int categoryId)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.CategoryId = categoryId;
+    }
+
+}
