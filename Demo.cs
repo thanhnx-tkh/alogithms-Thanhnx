409d7ff141cff0b167f8e3b232ded4fbaf391ff8
thanhnx <thanhnx.tkh@gmail.com>
2024-09-11 10:49:11 +0700

    + ValidExpression

--- Demo.cs
+++ Demo.cs
@@ -42,101 +42,101 @@ public class Demo
         Console.WriteLine("---------------------------------------");
     }
 
-    static void Main(string[] args)
-    {
-        //List<Product> products = new List<Product>();
+    //static void Main(string[] args)
+    //{
+    //    //List<Product> products = new List<Product>();
 
-        //products.Add(new Product("RAM", 50, 2, 2));
-        //products.Add(new Product("HDD", 70, 1, 2));
-        //products.Add(new Product("CPU", 750, 10, 1));
-        //products.Add(new Product("Main", 400, 3, 1));
-        //products.Add(new Product("Keyboard", 30, 8, 4));
-        //products.Add(new Product("Mouse", 25, 50, 4));
-        //products.Add(new Product("VGA", 60, 35, 3));
-        //products.Add(new Product("Monitor", 120, 28, 2));
-        //products.Add(new Product("Case", 120, 28, 5));    
-
-
-
-        //List<Category> categories = new List<Category>();
-        //categories.Add(new Category(1, "Computer"));
-        //categories.Add(new Category(2, "Memory"));
-        //categories.Add(new Category(3, "Card"));
-        //categories.Add(new Category(4, "Accesory"));
-        //categories.Add(new Category(5, "Base"));
+    //    //products.Add(new Product("RAM", 50, 2, 2));
+    //    //products.Add(new Product("HDD", 70, 1, 2));
+    //    //products.Add(new Product("CPU", 750, 10, 1));
+    //    //products.Add(new Product("Main", 400, 3, 1));
+    //    //products.Add(new Product("Keyboard", 30, 8, 4));
+    //    //products.Add(new Product("Mouse", 25, 50, 4));
+    //    //products.Add(new Product("VGA", 60, 35, 3));
+    //    //products.Add(new Product("Monitor", 120, 28, 2));
+    //    //products.Add(new Product("Case", 120, 28, 5));    
 
 
-        //Print(products);
-        //Print(categories);
-        //Print(Bai11BubbleSort.SortByPrice(products));
-        //Print(Bai12InsertionSort.SortByName(products));
-        //Print(Bai13InsertionSort.SortByCategoryName(products, categories), categories);
-        //Print1(Bai14MapProductByCategory.MapProductByCategory(products, categories));
-        //Console.WriteLine(Bai15MinByPrice.MinByPrice(products).Price);
-        //Console.WriteLine(Bai16MaxByPrice.MaxByPrice(products).Price);
-        //Console.WriteLine(Bai21CalSalary.CalSalaryDeQuy(20, 4));
-        //Console.WriteLine(Bai21CalSalary.CalSalaryDeQuy(20, 4));
 
-        //Console.WriteLine(Bai22CalMonth.CalMonth(20, 5));
-        //Console.WriteLine(Bai22CalMonth.CalMonthDeQuy(10000, 80));
+    //    //List<Category> categories = new List<Category>();
+    //    //categories.Add(new Category(1, "Computer"));
+    //    //categories.Add(new Category(2, "Memory"));
+    //    //categories.Add(new Category(3, "Card"));
+    //    //categories.Add(new Category(4, "Accesory"));
+    //    //categories.Add(new Category(5, "Base"));
 
 
-        //Menu[] menus = new Menu[]
-        //{
-        //    new Menu(1, "Thể thao", 0),
-        //    new Menu(2, "Xã hội", 0),
-        //    new Menu(3, "Thể thao trong nước", 1),
-        //    new Menu(4, "Giao thông", 2),
-        //    new Menu(5, "Môi trường", 2),
-        //    new Menu(6, "Thể thao quốc tế", 1),
-        //    new Menu(7, "Môi trường đô thị", 5),
-        //    new Menu(8, "Giao thông ùn tắc", 4)
-        //};
+    //    //Print(products);
+    //    //Print(categories);
+    //    //Print(Bai11BubbleSort.SortByPrice(products));
+    //    //Print(Bai12InsertionSort.SortByName(products));
+    //    //Print(Bai13InsertionSort.SortByCategoryName(products, categories), categories);
+    //    //Print1(Bai14MapProductByCategory.MapProductByCategory(products, categories));
+    //    //Console.WriteLine(Bai15MinByPrice.MinByPrice(products).Price);
+    //    //Console.WriteLine(Bai16MaxByPrice.MaxByPrice(products).Price);
+    //    //Console.WriteLine(Bai21CalSalary.CalSalaryDeQuy(20, 4));
+    //    //Console.WriteLine(Bai21CalSalary.CalSalaryDeQuy(20, 4));
 
-        //Bai23PrintMenu.PrintMenu(menus);
-
-        //Queue<int> queue = new Queue<int>();
-        //queue.Push(1);
-        //queue.Push(2);
-        //queue.Push(3);
-
-        //Console.WriteLine("--------Queue-------");
+    //    //Console.WriteLine(Bai22CalMonth.CalMonth(20, 5));
+    //    //Console.WriteLine(Bai22CalMonth.CalMonthDeQuy(10000, 80));
 
 
-        //Console.WriteLine(queue.Get());
-        //Console.WriteLine(queue.Get());
-        //Console.WriteLine(queue.Get());
+    //    //Menu[] menus = new Menu[]
+    //    //{
+    //    //    new Menu(1, "Thể thao", 0),
+    //    //    new Menu(2, "Xã hội", 0),
+    //    //    new Menu(3, "Thể thao trong nước", 1),
+    //    //    new Menu(4, "Giao thông", 2),
+    //    //    new Menu(5, "Môi trường", 2),
+    //    //    new Menu(6, "Thể thao quốc tế", 1),
+    //    //    new Menu(7, "Môi trường đô thị", 5),
+    //    //    new Menu(8, "Giao thông ùn tắc", 4)
+    //    //};
+
+    //    //Bai23PrintMenu.PrintMenu(menus);
+
+    //    //Queue<int> queue = new Queue<int>();
+    //    //queue.Push(1);
+    //    //queue.Push(2);
+    //    //queue.Push(3);
+
+    //    //Console.WriteLine("--------Queue-------");
 
 
-        //Stack<int> stack = new Stack<int>();
+    //    //Console.WriteLine(queue.Get());
+    //    //Console.WriteLine(queue.Get());
+    //    //Console.WriteLine(queue.Get());
 
-        //stack.Push(1);
-        //stack.Push(2);
-        //stack.Push(3);
 
-        //Console.WriteLine("--------Stack-------");
+    //    //Stack<int> stack = new Stack<int>();
 
-        //Console.WriteLine(stack.Get());
-        //Console.WriteLine(stack.Get());
-        //Console.WriteLine(stack.Get());
+    //    //stack.Push(1);
+    //    //stack.Push(2);
+    //    //stack.Push(3);
 
-        // Dữ liệu mẫu menu
-        //List<Menu> menus = new List<Menu>
-        //{
-        //    new Menu { Id = 1, Title = "Thể thao", ParentId = 0 },
-        //    new Menu { Id = 2, Title = "Xã hội", ParentId = 0 },
-        //    new Menu { Id = 3, Title = "Thể thao trong nước", ParentId = 1 },
-        //    new Menu { Id = 4, Title = "Giao thông", ParentId = 2 },
-        //    new Menu { Id = 5, Title = "Môi trường", ParentId = 2 },
-        //    new Menu { Id = 6, Title = "Thể thao quốc tế", ParentId = 1 },
-        //    new Menu { Id = 7, Title = "Môi trường đô thị", ParentId = 5 },
-        //    new Menu { Id = 8, Title = "Giao thông ùn tắc", ParentId = 4 }
-        //};
+    //    //Console.WriteLine("--------Stack-------");
 
-        //Bai23PrintMenu.PrintMenu(menus);
-        Console.ReadLine();
+    //    //Console.WriteLine(stack.Get());
+    //    //Console.WriteLine(stack.Get());
+    //    //Console.WriteLine(stack.Get());
 
-    }
+    //    // Dữ liệu mẫu menu
+    //    //List<Menu> menus = new List<Menu>
+    //    //{
+    //    //    new Menu { Id = 1, Title = "Thể thao", ParentId = 0 },
+    //    //    new Menu { Id = 2, Title = "Xã hội", ParentId = 0 },
+    //    //    new Menu { Id = 3, Title = "Thể thao trong nước", ParentId = 1 },
+    //    //    new Menu { Id = 4, Title = "Giao thông", ParentId = 2 },
+    //    //    new Menu { Id = 5, Title = "Môi trường", ParentId = 2 },
+    //    //    new Menu { Id = 6, Title = "Thể thao quốc tế", ParentId = 1 },
+    //    //    new Menu { Id = 7, Title = "Môi trường đô thị", ParentId = 5 },
+    //    //    new Menu { Id = 8, Title = "Giao thông ùn tắc", ParentId = 4 }
+    //    //};
+
+    //    //Bai23PrintMenu.PrintMenu(menus);
+    //    Console.ReadLine();
+
+    //}
 
 }
 
---
+++ ValidExpression.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ValidExpression
+{
+    public static bool IsValidExpression(string expression)
+    {
+        char[] characters = expression.ToCharArray();
+
+        Stack<char> stack = new Stack<char>();
+
+        for (int i = 0; i < characters.Length; i++)
+        {
+            stack.Push(characters[i]);
+        }
+
+        int countParenthesis = 0;
+        int lengthStack = stack.Count;
+
+        for (int i = 0; i < lengthStack; i++)
+        {
+            if (stack.Peek() == '(')
+            {
+                countParenthesis++;
+                stack.Pop();
+            }
+            else if (stack.Peek() == ')')
+            {
+                countParenthesis--;
+                stack.Pop();
+            }
+        }
+        if (countParenthesis == 0) return true;
+        return false;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsValidExpression(new string('(',5) + new string(')',6)));
+        Console.WriteLine(IsValidExpression(new string('(', 10000) + new string(')', 10000)));
+        Console.ReadLine();
+    }
+}
+
